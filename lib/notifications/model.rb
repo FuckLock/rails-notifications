7c1dd19b076138a3295406302a4e0216ba16962a
baodongdong <baodongdong@tigerjoys.com>
2018-01-18 18:54:02 +0800

    代码稍作调整

--- lib/notifications/model.rb
+++ lib/notifications/model.rb
@@ -10,19 +10,20 @@ module Notifications
       belongs_to :target, class_name: Notifications.config.object_class
       belongs_to :ancestry, polymorphic: true
       belongs_to :second_ancestry, polymorphic: true
+      belongs_to :third_ancestry, polymorphic: true
 
       # define has_many relationships
       Object.const_get(Notifications.config.object_class).class_eval do
-        options = {}
-        config = Notifications.config
-        notify_type = config.notify_type
-        ancestry_type = config.ancestry_type
-        second_ancestry_type = config.second_ancestry_type
-        third_ancestry_type = config.third_ancestry_type
-        options[:notify_type] = notify_type
-        options[:ancestry_type] =  ancestry_type if ancestry_type.present?
-        options[:second_ancestry_type] =  second_ancestry_type if second_ancestry_type..present?
-        options[:third_ancestry_type] =  third_ancestry_type if third_ancestry_type.present?
+        options                        = {}
+        config                         = Notifications.config
+        notify_type                    = config.notify_type
+        ancestry_type                  = config.ancestry_type
+        second_ancestry_type           = config.second_ancestry_type
+        third_ancestry_type            = config.third_ancestry_type
+        options[:notify_type]          = notify_type
+        options[:ancestry_type]        = ancestry_type        if ancestry_type.present?
+        options[:second_ancestry_type] = second_ancestry_type if second_ancestry_type.present?
+        options[:third_ancestry_type]  = third_ancestry_type  if third_ancestry_type.present?
         has_many :notifications, -> { where(options) }, foreign_key: :target_id, dependent: :destroy
         has_many :subjects, through: :notifications
       end
--- lib/rails-notifications.rb
+++ lib/rails-notifications.rb
@@ -13,14 +13,14 @@ module Notifications
     @config.notify_type            = 'mention'
     @config.ancestry_type          = ''
     @config.second_ancestry_type   = ''
-    @config.third_ancestry_type   = ''
+    @config.third_ancestry_type    = ''
     @config.current_user_method    = 'current_user'
-    @config.user_avatar_method = ''
+    @config.user_avatar_method     = ''
     @config
   end
 
   def configure(&block)
     config.instance_exec(&block)
   end
-
+  
 end
